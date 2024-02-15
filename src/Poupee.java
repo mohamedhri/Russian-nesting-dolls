d751649e922c98a308413dc81ad88047412735df
mohamedhri <125465245+mohamedhri@users.noreply.github.com>
2024-02-15 15:01:53 +0100

    class poupee

--- src/Poupee.java
+++ src/Poupee.java
@@ -1,7 +1,31 @@
 package src;
-
+import java.util.Scanner;
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
-public class Poupee {
+ abstract class Poupee {
+    private   int taille ;
+    private boolean ouverte;
+
+    public Poupee(int taille){
+        this.taille=taille;
+        this.ouverte=false;
+    }
+    public int getTaille(){
+        return taille;
+    }
+    public void ouvrir() {
+        ouverte= true;
+        System.out.println("la poupee est ouverte .");
+
+    }
+    public void fermer (){
+        ouverte=false;
+        System.out.println("la poupee est fermee");
+    }
+    public abstract void placerDans(Poupee a);
+    public abstract void sortirDe (Poupee a);
+
+
+
     
 }
