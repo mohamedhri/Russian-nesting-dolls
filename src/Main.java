f7ec77cfad3a6985a2a9ba6aa9f476899e7be5ed
mohamedhri <125465245+mohamedhri@users.noreply.github.com>
2024-02-16 16:28:48 +0100

    update the dolls class& add the Menu

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,32 @@
+// Main.java
 package src;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-    }
-}
+        PoupeeRusse firstDoll = new PoupeeRusse(10);
+        PoupeeRusse secondDoll = new PoupeeRusse(30);
+        PoupeeRusse thirdDoll = new PoupeeRusse(20);
+
+        int choice;
+
+        private static void printMenu() {
+            System.out.println("Menu :");
+            System.out.println("1. Jouer avec les poupées");
+            System.out.println("2. Afficher les informations des poupées");
+            System.out.println("3. Placer une poupée dans une autre (1ère poupée dans 2ème poupée)");
+            System.out.println("4. Placer une poupée dans une autre (2ème poupée dans 3ème poupée)");
+            System.out.println("5. Quitter");
+            System.out.print("Choix : ");
+        }
+
+        do {
+            printMenu();
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                 
--- src/Poupee.java
+++ src/Poupee.java
@@ -1,31 +1,39 @@
-package src;
-import java.util.Scanner;
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
- abstract class Poupee {
-    private   int taille ;
+package src;
+
+
+public abstract class Poupee {
+    private int taille;
     private boolean ouverte;
 
-    public Poupee(int taille){
-        this.taille=taille;
-        this.ouverte=false;
+    public Poupee(int taille) {
+        this.taille = taille;
+        this.ouverte = false;
     }
-    public int getTaille(){
+
+
+    public int getTaille() {
+
         return taille;
     }
-    public void ouvrir() {
-        ouverte= true;
-        System.out.println("la poupee est ouverte .");
-
+    public boolean isOuverte (){
+        return ouverte;
     }
-    public void fermer (){
-        ouverte=false;
-        System.out.println("la poupee est fermee");
+    public void setOuverte (boolean ouverte){
+        this.ouverte=ouverte;
     }
-    public abstract void placerDans(Poupee a);
-    public abstract void sortirDe (Poupee a);
+
+    public boolean estOuverte() {
+        return ouverte;
+    }
+
+    public abstract void ouvrir();
 
 
+    public abstract void fermer();
 
-    
+
+    public abstract void placerDans(Poupee p);
+
+    public abstract void sortirDe(Poupee p);
 }
